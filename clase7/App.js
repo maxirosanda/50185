235e1b150ab6d0f84daf63aafd3e342a9ed2a6e1
maxirosanda <maxi_rosanda@hotmail.com>
2023-12-06 22:40:43 -0300

    clase 7

---
+++ clase7/App.js
@@ -0,0 +1,41 @@
+import { StyleSheet, View } from 'react-native'
+import Home from './src/Screens/Home'
+import ItemListCategories from './src/Screens/ItemListCategories'
+import ItemDetail from './src/Screens/ItemDetail'
+import { useState } from 'react'
+import { useFonts } from "expo-font"
+
+const  App = () => {
+  const [categorySelected,setCategorySelected] = useState("")
+  
+  const [fontLoaded] = useFonts({
+    Josefin:require("./assets/Fonts/JosefinSans-Bold.ttf")
+  })
+
+  if(!fontLoaded) return null
+
+  
+  return (
+    <View style={styles.container}>
+      {categorySelected ?
+        <ItemListCategories category = {categorySelected}/>
+        :
+        <Home setCategorySelected={setCategorySelected}/>
+      }
+     
+    </View>
+  )
+}
+
+export default App
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'start',
+  },
+})
+
