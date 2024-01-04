65a85140efc3fe2fc4c178734f31786f6a05a005
maxirosanda <maxi_rosanda@hotmail.com>
2024-01-03 22:32:57 -0300

    clase 12

---
+++ clase12 03-01/App.js
@@ -0,0 +1,37 @@
+import { StyleSheet,StatusBar } from 'react-native'
+import { useFonts } from "expo-font"
+import { colors } from './src/Global/colors'
+import { fonts } from './src/Global/fonts'
+import TabNavigator from './src/navigation/TabNavigator'
+import { store } from './src/app/store'
+import { Provider } from 'react-redux'
+
+
+
+const  App = () => {
+
+  const [fontLoaded] = useFonts(fonts)
+  if(!fontLoaded) return null
+  
+  return (
+    <>
+      <StatusBar backgroundColor={colors.green1}/>
+      <Provider store={store}>
+        <TabNavigator/>
+      </Provider>
+    
+    </>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'start',
+  },
+})
+
